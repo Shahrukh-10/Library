b208ab11f3849ce31c77742f4f425cad7f91c856
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2021-08-08 16:22:47 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,38 +9,7 @@ class Book {
 }
 
 class Display {
-    add(book) {
-        console.log("adding book ...");
-        let tableBody = document.getElementById("tableBody");
-        let savedBooks = localStorage.getItem('books');
-        if (savedBooks == null) {
-            bookObj = [];
-        } else {
-            bookObj = savedBooks.parse();
-        }
-        savedBooks = localStorage.getItem('savedBooks');
-        if (savedBooks == null) {
-            bookObj = [];
-        } else {
-            bookObj = JSON.parse(savedBooks);
-        }
-
-        let uiString = "";
-
-        bookObj.forEach((book) => {
-
-            uiString += `
-            <tr>
-            
-            <td>${book.name} </td>
-            <td>${book.author}</td>
-            <td>${book.type} </td>
-            </tr>
-            `;
-
-        });
-        tableBody.innerHTML = uiString;
-    }
+  
 
     clear() {
         let libraryForm = document.getElementById("libraryForm");
@@ -67,9 +36,36 @@ class Display {
         setTimeout(() => {
             message.innerHTML = "";
         }, 5000);
+        showBooks();
     }
 }
+function showBooks() {
+    let savedBooks = localStorage.getItem('savedBooks');
+    console.log("adding book ...");
+    let tableBody = document.getElementById("tableBody");
+    if (savedBooks == null) {
+        bookObj = [];
+    } else {
+        bookObj = JSON.parse(savedBooks);
+    }
 
+    let uiString = "";
+
+    bookObj.forEach((book) => {
+
+        uiString += `
+        <tr>
+        
+        <td>${book.name} </td>
+        <td>${book.author}</td>
+        <td>${book.type} </td>
+        </tr>
+        `;
+
+    });
+    tableBody.innerHTML = uiString;
+}
+showBooks();
 let libraryForm = document.getElementById("libraryForm");
 libraryForm.addEventListener('submit', libraryFormSubmit);
 
@@ -92,36 +88,29 @@ function libraryFormSubmit(e) {
     else if (soccer.checked) {
         type = soccer.value;
     }
-
-
-
+    
+    
+    
     let book = new Book(name, author, type);
     console.log(book);
-
-    let savedBooks = localStorage.getItem('savedBooks');
+    let savedBooks = localStorage.getItem("savedBooks");
     if (savedBooks == null) {
         bookObj = [];
-        console.log("1")
     } else {
-        console.log("2")
         bookObj = JSON.parse(savedBooks);
     }
     bookObj.push(book);
-    console.log("3")
     localStorage.setItem("savedBooks", JSON.stringify(bookObj));
-    console.log("4")
-
+    
     let display = new Display();
-
+    
     if (display.validate(book)) {
-        display.add(book);
         display.clear();
         display.show('success', "Your book has been successfully added.");
     }
     else {
         display.show('danger', "Sorry , You cannot add this book !");
     }
-
     e.preventDefault();
 
 }
